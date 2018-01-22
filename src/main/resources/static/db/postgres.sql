b60db8efe0f7501efa3346505ff6079ddc2c485d
koyunmania <vv1357@hotmail.com>
2018-01-23 00:07:50 +0100

    - DB update

--- src/main/resources/static/db/postgres.sql
+++ src/main/resources/static/db/postgres.sql
@@ -1,62 +1,39 @@
-CREATE TABLE users(
-   user_id SERIAL PRIMARY KEY,
-   username varchar(20) NOT NULL,
-   password varchar(20) NOT NULL,
-   enabled boolean NOT NULL DEFAULT FALSE
-   -- CONSTRAINT users_pkey PRIMARY KEY (user_id)
+CREATE TABLE user_profile
+(
+  user_id serial NOT NULL,
+  username character varying(20),
+  password character varying(20) NOT NULL,
+  enabled boolean NOT NULL DEFAULT false,
+  email character varying(30) NOT NULL,
+  CONSTRAINT user_profile_pkey PRIMARY KEY (user_id)
 );
 
-create table roles (
-   role_id SERIAL PRIMARY KEY,
-   role varchar(20) NOT NULL
-   -- CONSTRAINT roles_pkey PRIMARY KEY (role_id)
+CREATE TABLE role
+(
+  role_id serial NOT NULL,
+  role_name character varying(20) NOT NULL,
+  CONSTRAINT role_pkey PRIMARY KEY (role_id)
 );
 
-create table user_roles (
-   user_id integer NOT NULL,
-   role_id integer NOT NULL,
-   CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role_id),
-   FOREIGN KEY (user_id) REFERENCES users (user_id),
-   FOREIGN KEY (role_id) REFERENCES roles (role_id)
+CREATE TABLE user_role
+(
+  user_id integer NOT NULL,
+  role_id integer NOT NULL,
+  CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role_id),
+  CONSTRAINT user_role_role_id_fkey FOREIGN KEY (role_id)
+      REFERENCES role (role_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT user_role_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES user_profile (user_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-INSERT INTO users(username,password,enabled) VALUES ('jack','jack', true);
-INSERT INTO users(username,password,enabled) VALUES ('peter','peter', true);
+INSERT INTO user_profile(username,password,enabled,email) VALUES ('jack','jack', true,'j@j.com');
+INSERT INTO user_profile(username,password,enabled,email) VALUES ('peter','peter', true,'p@p.com');
  
-INSERT INTO roles (role) VALUES ('ROLE_USER');
-INSERT INTO roles (role) VALUES ('ROLE_ADMIN');
+INSERT INTO role (role_name) VALUES ('ROLE_USER');
+INSERT INTO role (role_name) VALUES ('ROLE_ADMIN');
 
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 2);
-INSERT INTO user_roles (user_id, role_id) VALUES (2, 1);
-
-
-
--------------------------
-
-CREATE TABLE users(
-   username varchar(20) NOT NULL,
-   password varchar(20) NOT NULL,
-   enabled boolean NOT NULL DEFAULT FALSE,
-   primary key(username)
-);
-
-create table user_roles (
-  user_role_id SERIAL PRIMARY KEY,
-  username varchar(20) NOT NULL,
-  role varchar(20) NOT NULL,
-  UNIQUE (username,role),
-  FOREIGN KEY (username) REFERENCES users (username)
-);
-
-INSERT INTO users(username,password,enabled) VALUES ('jack','jack', true);
-INSERT INTO users(username,password,enabled) VALUES ('peter','peter', true);
- 
-INSERT INTO user_roles (username, role) VALUES ('jack', 'ROLE_USER');
-INSERT INTO user_roles (username, role) VALUES ('jack', 'ROLE_ADMIN');
-INSERT INTO user_roles (username, role) VALUES ('peter', 'ROLE_USER');
-
-
-
--------------------------
-
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (1, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
